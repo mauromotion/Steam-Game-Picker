e095709c92a118896d3c9a9301c1c70dbce8f035
mauromotion <hello@mauromotion.com>
2023-08-26 22:49:00 +0100

    worked on retrieving data from the json file from the API

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import random
 from steam import Steam
 from decouple import config
 
@@ -5,21 +6,66 @@ KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
 
 
+# Get data from Steam API
 def get_steam_data(username):
 
     userinfo = steam.users.search_user(username)
-    print(userinfo)
+    # print(userinfo)
 
     steam_id = userinfo['player']['steamid']
-    print(steam_id)
+    nickname = userinfo['player']['personaname']
+    tot_games = steam.users.get_owned_games(steam_id)['game_count']
+    avatar = userinfo['player']['avatarfull']
+    # print(steam_id)
 
-    owned_games = steam.users.get_owned_games(steam_id)
-    print(owned_games)
+    owned_games = steam.users.get_owned_games(steam_id)['games']
+    # print(owned_games)
 
-    terraria = steam.apps.search_games("terraria")
-    print(terraria)
+    # terraria = steam.apps.search_games("terraria")
+    # print(terraria)
 
-    return owned_games
+    library = []
+
+    for game in owned_games:
+        new_game = {}
+
+        appid = game['appid']
+        url_base = 'https://store.steampowered.com/app/{}'
+        url = url_base.format(appid)
+
+        try:
+            description = steam.apps.get_app_details(
+                appid)[str(appid)]['data']['short_description']
+        except KeyError:
+            description = 'No description'
+
+        try:
+            genres = steam.apps.get_app_details(
+                appid)[str(appid)]['data']['genres']['description']
+        except KeyError:
+            genres = 'No data'
+
+        new_game['appid'] = game['appid']
+        new_game['name'] = game['name']
+        new_game['url'] = url
+        new_game['genres'] = genres
+        new_game['description'] = description
+        new_game['playtime'] = round((game['playtime_forever'] / 60), 1)
+
+        library.append(new_game)
+
+    user_data = {
+        'nickname': nickname,
+        'tot_games': tot_games,
+        'avatar': avatar
+    }
+
+    return library, user_data
+
+
+# Generate a random integer between 1 and the argument
+def random_in_list(len):
+    return random.randrange(1, len)
 
 
 '''
---
+++ testing/test_random.py
@@ -0,0 +1,8 @@
+import random
+
+
+def random_in_list(len):
+    return random.randrange(1, len)
+
+
+print(random_in_list(200))
