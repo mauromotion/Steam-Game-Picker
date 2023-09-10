fcd067425b428e2a03978cb3d25390c8a7b74841
mauromotion <hello@mauromotion.com>
2023-09-10 16:12:33 +0100

    finished the fronted

--- queries.sql
+++
@@ -1,8 +0,0 @@
-query = "SELECT id, appid, name, playtime FROM library"
-query = "SELECT id, appid, name, playtime FROM library WHERE playtime = 0"
-query = "SELECT id, appid, name, playtime FROM library WHERE playtime != 0"
-query = "SELECT id, appid, name, playtime FROM library ORDER BY playtime DESC LIMIT 5"
-query = "SELECT id, appid, name, playtime FROM library ORDER BY playtime DESC LIMIT 10"
-query = "SELECT id, appid, name, playtime FROM library ORDER BY playtime DESC LIMIT 20"
-
-
